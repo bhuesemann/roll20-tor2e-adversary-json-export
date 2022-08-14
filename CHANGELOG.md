9fdad6a1900177fbbc67614ad9d78ad253c1ff87
Bodo Huesemann <bhuesemann@users.noreply.github.com>
2022-08-14 21:11:11 +0200

    added startup and progress messages

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,3 +9,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 This is the initial release of the project. Please note that this is an beta release to get first feedback. 
 ### Added
 - Everything
+
+## [0.1.1] - 2022-08-14
+### Added
+- Startup and progress messages
--- Program.cs
+++ Program.cs
@@ -11,50 +11,61 @@ namespace roll20_adv_import_c
     {
         static void Main(string[] args)
         {   
+            Console.WriteLine("This tool will convert the PDF files found in path ./pdf and create JSON files in ./out!");
             string[] filePaths = Directory.GetFiles(@"./pdf", "*.pdf", SearchOption.AllDirectories);
-            foreach (string pdfPath in filePaths){
-                // pdf conversion 
-                string basename = Path.GetFileName(pdfPath).Replace(Path.GetExtension(pdfPath), "");
-                string txtPath = @$"./out/{basename}.txt";
-                string jsonPath = @$"./out/{basename}.json";
-                PdfConverter.convert(pdfPath, txtPath);
-                string sanitized = TorAdvSanitizer.Sanitize(txtPath);
+            if (filePaths.Length == 0) {
+                Console.WriteLine("No files found to be processed!");
+            } else {
+                Console.WriteLine("Found files: " + filePaths.Length);
 
-                // parsing
-                Adversary[] parsed = null;
-                if (basename.Contains("Adversary")){
-                    TorAdvParserAdd.Init();
-                    parsed = TorAdvParserAdd.advs.Parse(sanitized);
-                } else {
-                    TorAdvParserCore.Init();
-                    parsed = TorAdvParserCore.advs.Parse(sanitized);
-                }
+                foreach (string pdfPath in filePaths){
+                    // pdf conversion 
+                    string basename = Path.GetFileName(pdfPath).Replace(Path.GetExtension(pdfPath), "");
+                    string txtPath = @$"./out/{basename}.txt";
+                    string jsonPath = @$"./out/{basename}.json";
 
-                // serialization (remove null properties)
-                JsonSerializerOptions jso = new JsonSerializerOptions
-                {
-                    WriteIndented = true,
-                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
-                };
-                string jsonString = JsonSerializer.Serialize(parsed, jso);
-                File.WriteAllText(jsonPath, jsonString);
+                    Console.WriteLine("Processing file: " + basename);    
 
-                // analyse result and provide feedback
-                HashSet<string> advs = parsed.Select(adv => adv.name).ToHashSet();
-                int found = 0;
-                List<string> missing = new List<string>{};            
-                int total = Config.AdversaryTokenList.Count;
-                foreach(String adv in Config.AdversaryTokenList){
-                    if (advs.Contains(adv)){
-                        found++;
+                    PdfConverter.convert(pdfPath, txtPath);
+                    string sanitized = TorAdvSanitizer.Sanitize(txtPath);
+
+                    // parsing
+                    Adversary[] parsed = null;
+                    if (basename.Contains("Adversary")){
+                        TorAdvParserAdd.Init();
+                        parsed = TorAdvParserAdd.advs.Parse(sanitized);
                     } else {
-                        missing.Add(adv);
+                        TorAdvParserCore.Init();
+                        parsed = TorAdvParserCore.advs.Parse(sanitized);
                     }
-                };
-                Console.WriteLine($"Parsed {found} / {total} adversaries.");
-                if (missing.Count > 0) {
-                    Console.WriteLine($"Missing: {string.Join(", ", missing)}");
+
+                    // serialization (remove null properties)
+                    JsonSerializerOptions jso = new JsonSerializerOptions
+                    {
+                        WriteIndented = true,
+                        Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                    };
+                    string jsonString = JsonSerializer.Serialize(parsed, jso);
+                    File.WriteAllText(jsonPath, jsonString);
+
+                    // analyse result and provide feedback
+                    HashSet<string> advs = parsed.Select(adv => adv.name).ToHashSet();
+                    int found = 0;
+                    List<string> missing = new List<string>{};            
+                    int total = Config.AdversaryTokenList.Count;
+                    foreach(String adv in Config.AdversaryTokenList){
+                        if (advs.Contains(adv)){
+                            found++;
+                        } else {
+                            missing.Add(adv);
+                        }
+                    };
+                    Console.WriteLine($"Parsed {found} / {total} adversaries.");
+                    if (missing.Count > 0) {
+                        Console.WriteLine($"Missing: {string.Join(", ", missing)}");
+                    }
                 }
+                Console.WriteLine("Processing completed!");
             }
         }
     }
